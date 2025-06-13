e4740fb52a7afaaa50464f8084033b1b8d7b5c5f
Ishaan290705 <ishaanbhindi@gmail.com>
2025-06-13 12:09:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import datetime
 # For Discourse fetching, filtering, and processing data
 DATE_FORMAT = "%Y-%m-%d"
 START_DATE = datetime.strptime("2025-01-01", DATE_FORMAT)
-END_DATE = datetime.strptime("2025-04-14", DATE_FORMAT)
+END_DATE = datetime.strptime("2025-06-13", DATE_FORMAT)
 # Number of discourse pages to scrape
 PAGES = 10
 PYTESSERACT_PATH = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -11,4 +11,4 @@ PYTESSERACT_PATH = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
 # For course content fetching, filtering, and processing data
-PLAYWRIGHT_BROWSERS_PATH = r"C:\Users\Lovep\miniconda3\playwright"
+PLAYWRIGHT_BROWSERS_PATH = r"E:\TDS\virtual-ta-tds\venv\Lib\site-packages"
